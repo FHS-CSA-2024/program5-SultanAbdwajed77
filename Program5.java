3c4a792a2e095013d3e2beb9d2f68d1367ff12cf
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-08-26 15:29:23 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -4,32 +4,41 @@ public class Program5 {
         String vehicle1 = "Royale";
         int miles1 = 286;
         int gallons1 = 9;
-
-        String vehicle2 = "Koopa King";
+        double RoyaleAvg = 0.0;
+        
+        String vehicle2 = "KoopaKing";
         int miles2 = 412;
         int gallons2 = 40;
-
-        String vehicle3 = "Pipe Frame";
+        double KoopaKingAvg = 0.0;
+        
+        String vehicle3 = "PipeFrame";
         int miles3 = 361;
         int gallons3 = 18;
-
+        double PipeFrameAvg = 0.0;
+        
         String vehicle4 = "Badwagon";
         int miles4 = 161;
         int gallons4 = 11;
+        double BadwagonAvg = 0.0;
+
+        
+        double RoyaleAvg = (double) miles1 / gallons1;
+        double KoopaKingAvg = (double) miles2 / gallons2;
+        double PipeFrameAvg = (double) miles3 / gallons3;
+        double BandwagonAvg = (double) miles4 / gallons4;
 
 
-        double mpg1 = (double) miles1 / gallons1;
-        double mpg2 = (double) miles2 / gallons2;
-        double mpg3 = (double) miles3 / gallons3;
-        double mpg4 = (double) miles4 / gallons4;
-
+        RoyaleAvg = ((int)((RoyaleAvg*10)+0.5))/10.0;
+        KoopaKingAvg = ((int)((KoopaKingAvg*10)+0.5))/10.0;
+        PipeFrameAvg = ((int)((PipeFrameAvg*10)+0.5))/10.0;
+        BandwagonAvg = ((int)((BandwagonAvg*10)+0.5))/10.0;
 
 
         System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon:");
-        System.out.println("Royale averaged double(mpg1)");
-        System.out.println("Koopa King averaged double(mpg2)");
-        System.out.println("Pipe Frame averaged double(mpg3)");
-        System.out.println("Badwagon averaged double(mpg4)");
+        System.out.println(vehicle1 + " averaged " + RoyaleAvg + " m/g");
+        System.out.println(vehicle2 + " averaged " + KoopaKingAvg + " m/g");
+        System.out.println(vehicle3 + " averaged " + PipeFrameAvg + " m/g");
+        System.out.println(vehicle4 + " averaged " + BandwagonAvg + " m/g");
     }
 }
 //Your code here
