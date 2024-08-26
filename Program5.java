d7a0edaf782a70378b02409c5d62a9ff3a2b64c9
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-08-25 20:55:32 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -1,5 +1,37 @@
 //import stuff here?
+public class VehicleMPGCalculator {
+    public static void main(String[] args) {
+        String vehicle1 = "Royale";
+        int miles1 = 286;
+        int gallons1 = 9;
 
+        String vehicle2 = "Koopa King";
+        int miles2 = 412;
+        int gallons2 = 40;
+
+        String vehicle3 = "Pipe Frame";
+        int miles3 = 361;
+        int gallons3 = 18;
+
+        String vehicle4 = "Badwagon";
+        int miles4 = 161;
+        int gallons4 = 11;
+
+
+        double mpg1 = (double) miles1 / gallons1;
+        double mpg2 = (double) miles2 / gallons2;
+        double mpg3 = (double) miles3 / gallons3;
+        double mpg4 = (double) miles4 / gallons4;
+
+
+
+        System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon:");
+        System.out.println("Royale averaged double(mpg1)");
+        System.out.println("Koopa King averaged double(mpg2)");
+        System.out.println("Pipe Frame averaged double(mpg3)");
+        System.out.println("Badwagon averaged double(mpg4)");
+    }
+}
 //Your code here
 
 //Paste console output below:
@@ -7,3 +39,4 @@
 
 
 */
+
